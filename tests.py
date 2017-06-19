a46b3a4f537a429ced3db6a26eb72d7c5128aa31
szostiPL <drizz52@gmail.com>
2017-06-19 17:17:06 +0200

    test_create_square method test implemented

--- tests.py
+++ tests.py
@@ -7,6 +7,10 @@ class TestDrawingMethods(unittest.TestCase):
         y = (2,2)
         line = create_line(x,y)
         self.assertEqual(line, [(1,1),(2,2),(3,3),(4,4)])
+    
+    def test_create_square(self):
+        square = create_square()
+        self.assertEqual(square, [(0,0),(4,0),(0,4),(4,4)])
 
 if __name__ == "__main__":
     unittest.main()
