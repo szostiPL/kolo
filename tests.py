511e7e5ce444be9f22ec6afe695580a3542c2a04
szostiPL <drizz52@gmail.com>
2017-06-19 17:28:22 +0200

    test_measure_angle method test implemented

--- tests.py
+++ tests.py
@@ -12,5 +12,15 @@ class TestDrawingMethods(unittest.TestCase):
         square = create_square()
         self.assertEqual(square, [(0,0),(4,0),(0,4),(4,4)])
 
+    def test_measure_angle(self):
+        x = (1,1)
+        y = (2,2)
+        wall_x = (0,0)
+        wall_y = (4,0)
+        line = create_line(x,y)
+        wall = create_line(wall_x, wall_y)
+        angle = measure_angle(line, wall)
+        self.assertEqual(angle, 43)
+
 if __name__ == "__main__":
     unittest.main()
