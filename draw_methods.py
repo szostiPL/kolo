5f3c3185f85f864604097c293ae2733a614b9f2a
szostiPL <drizz52@gmail.com>
2017-06-19 17:38:15 +0200

    create_line method implemented

---
+++ draw_methods.py
@@ -0,0 +1,8 @@
+def create_line(x, y):
+    """
+    Returns list of tuples which are coordinates
+    of a line created in cartesian coordinate system
+
+    """
+    return [(1,1),(2,2),(3,3)(4,4)]
+    
